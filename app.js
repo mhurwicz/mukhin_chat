9826c05b556f0c5e580dfc98bb3604c64c9c7503
Michael Hurwicz <mhurwicz@gmail.com>
2014-02-27 10:50:42 -0800

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
 
-server.listen(8080);
+server.listen();
 
 // routing
 app.get('/', function (req, res) {
